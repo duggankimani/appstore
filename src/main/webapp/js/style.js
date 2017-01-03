81504b0083ae3c2ee7be2e6b14513348ffa4a479
Patrick Chieni <pchieni25@gmail.com>
2017-01-03 15:42:45 +0300

    Load Available Processes from Backend JSON

--- src/main/webapp/js/style.js
+++ src/main/webapp/js/style.js
@@ -4,81 +4,167 @@ $('.carousel.carousel-slider').carousel({
 
 $('select').material_select('destroy');
 
+$(".ExistingDataForm").on("submit", function(e) {
 
-$(".ExistingDataForm").on("submit", function (e) {
-    
-	//var data = JSON.stringify( $('#ExistingDataForm').serializeArray() );
-	var data = $('#ExistingDataForm').serialize() ;
-	
+	// var data = JSON.stringify( $('#ExistingDataForm').serializeArray() );
+	var data = $('#ExistingDataForm').serialize();
+
 	e.preventDefault();
 	$.ajax({
-		url : 'api/upload/process',
-		contentType: 'multipart/form-data',
-	    type: 'POST',
-	    data : data,
+		url : 'api/upload/process/',
+		contentType : 'multipart/form-data',
+		type : 'POST',
+		data : data,
 		success : function(data) {
 
 			alert(data);
 
 		},
-		fail : function() {
+		fail : function(e) {
 
 			// showPleaseWait(false);
+			alert("Failed" + e);
 
 		},
 		error : function(XMLHttpRequest, textStatus, errorThrown) {
 
 			// showPleaseWait(false);
+			// showPleaseWait(false);
+			alert("Failed" + errorThrown);
 
 		}
 	});
 
 });
 
+$(document)
+		.ready(
+				function() {
 
-$(document).ready(function() {
+					$('select').material_select();
 
-	$('select').material_select();
+					/*
+					 * var jsonProcesses = '{"refId":"Z3KVjUt31OVrmcJc", +
+					 * '"name":"Payment Requests",' + '"description":"<p> This
+					 * proces ...[2000]<p>"}' + '"iconstyle":"icon-cogs",' +
+					 * '"processIcon" :
+					 * "http://wira.appstore.co.ke/downloads/Z3KVjUt31OVrmcJc/icon.png",' +
+					 * '"backgroundcolor":"red",' + '"status":[0,1],' +
+					 * '"category":"Finance",' +
+					 * '"downloadurl":"http://wira.appstore.co.ke/downloads/Z3KVjUt31OVrmcJc/Payment_Requests.zip",' +
+					 * '"images":[/downloads/Z3KVjUt31OVrmcJc/image1.png,' +
+					 * '/downloads/Z3KVjUt31OVrmcJc/image2.png,' +
+					 * '/downloads/Z3KVjUt31OVrmcJc/image3.png]}';
+					 * 
+					 * var obj = $.parseJSON(jsonProcesses); for ( var i in obj) {
+					 * //alert(obj[i]); }
+					 * 
+					 */
 
-	/*
-	 * var jsonProcesses = '{"refId":"Z3KVjUt31OVrmcJc", + '"name":"Payment
-	 * Requests",' + '"description":"<p> This proces ...[2000]<p>"}' +
-	 * '"iconstyle":"icon-cogs",' + '"processIcon" :
-	 * "http://wira.appstore.co.ke/downloads/Z3KVjUt31OVrmcJc/icon.png",' +
-	 * '"backgroundcolor":"red",' + '"status":[0,1],' + '"category":"Finance",' +
-	 * '"downloadurl":"http://wira.appstore.co.ke/downloads/Z3KVjUt31OVrmcJc/Payment_Requests.zip",' +
-	 * '"images":[/downloads/Z3KVjUt31OVrmcJc/image1.png,' +
-	 * '/downloads/Z3KVjUt31OVrmcJc/image2.png,' +
-	 * '/downloads/Z3KVjUt31OVrmcJc/image3.png]}';
-	 * 
-	 * var obj = $.parseJSON(jsonProcesses); for ( var i in obj) {
-	 * //alert(obj[i]); }
-	 * 
-	 */
+					$
+							.ajax({
+								url : 'api/processes',
+								async : false,
+								success : function(data) {
 
-	$.ajax({
-		url : 'json/process.json',
-		async : false,
-		success : function(data) {
+									$("#available_processes").html("");
 
-		},
-		fail : function() {
-			alert("f");
-		},
-		complete : function(data) {
-			// alert("c");
+									var htmlDataActive = "";
 
-			/*
-			 * var t = data; var data = t.replace(/&quot;/ig,'"'); dataRes =
-			 * jQuery.parseJSON(data); var $mainDetailsArr = dataRes[0];
-			 * alert($mainDetailsArr);
-			 */
+									// Header Definition
+									htmlDataActive = "<div class='col s12 m10'><h5>Available Processes</h5><p><span>" + data.length + "</span> Processes</p></div>";
 
-			// $("#spotlightList").html($htmlSpotlightYoutube);
-		},
-		error : function(XMLHttpRequest, textStatus, errorThrown) {
+									// Header Definition
+									htmlDataInActive = "<div class='col s12 m10'><h5>Upcoming Processes</h5><p><span>0</span> Processes</p></div>";
 
-		}
-	});
+									
+									for ( var items in data) {
 
-});
+										var refId = data[items]['refId'];
+										var isActive = data[items]['isActive'];
+										var id = data[items]['id'];
+										var name = data[items]['name'];
+										var description = data[items]['description'];
+										var iconStyle = data[items]['iconStyle'];
+										var backgroundColor = data[items]['backgroundColor'];
+										var processIcon = data[items]['processIcon'];
+										var category = data[items]['category'];
+										var status = data[items]['status'];
+										var classDeactivate = "";
+
+										if (isActive == "1") {
+
+											htmlDataActive += "<div class='col s12 m3 "
+													+ classDeactivate + "'>";
+											htmlDataActive += "<div class='card cards_listing'>";
+											htmlDataActive += "<p class='center listingsHeader'>"
+													+ name + "</p>"
+											htmlDataActive += "<h2 class='center light-blue-text'>"
+											htmlDataActive += "<i class='"
+													+ iconStyle + "'></i>";
+											htmlDataActive += "</h2>"
+											htmlDataActive += "<div class='card-content'>"
+											htmlDataActive += "<h5 class='center'>"
+													+ description + "</h5>"
+											htmlDataActive += "</div>";
+											htmlDataActive += "<div class='card-action'>";
+											htmlDataActive += "<a href='details.html'>VIEW</a><span class='free' href='#'>FREE</span>";
+											htmlDataActive += "</div></div></div>";
+
+											// classDeactivate = "deactivate";
+
+										} else if (isActive == "0") {
+
+											htmlDataInActive += "<div class='col s12 m3 "
+													+ classDeactivate + "'>";
+											htmlDataInActive += "<div class='card cards_listing'>";
+											htmlDataInActive += "<p class='center listingsHeader'>"
+													+ name + "</p>"
+											htmlDataInActive += "<h2 class='center light-blue-text'>"
+											htmlDataInActive += "<i class='"
+													+ iconStyle + "'></i>";
+											htmlDataInActive += "</h2>"
+											htmlDataInActive += "<div class='card-content'>"
+											htmlDataInActive += "<h5 class='center'>"
+													+ description + "</h5>"
+											htmlDataInActive += "</div>";
+											htmlDataInActive += "<div class='card-action'>";
+											htmlDataInActive += "<a href='details.html'>VIEW</a><span class='free' href='#'>FREE</span>";
+											htmlDataInActive += "</div></div></div>";
+
+										}
+
+									}
+
+									$("#available_processes").html("");
+									$("#upcoming_processes").html("");
+									
+									$("#available_processes").html(
+											htmlDataActive);
+									$("#upcoming_processes").html(
+											htmlDataInActive);
+
+								},
+								fail : function() {
+									alert("f");
+								},
+								complete : function(data) {
+									// alert("c");
+
+									/*
+									 * var t = data; var data =
+									 * t.replace(/&quot;/ig,'"'); dataRes =
+									 * jQuery.parseJSON(data); var
+									 * $mainDetailsArr = dataRes[0];
+									 * alert($mainDetailsArr);
+									 */
+
+									// $("#spotlightList").html($htmlSpotlightYoutube);
+								},
+								error : function(XMLHttpRequest, textStatus,
+										errorThrown) {
+
+								}
+							});
+
+				});
